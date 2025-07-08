f9a3dfd5d4491d6bab59cc158e476cf021cfbdf0
DaniilGordeev <gordeev.daniil.andreevic@gmail.com>
2025-07-09 02:18:49 +0500

    Frontend настроен и протестирован

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import type { ReactNode } from "react";
+
+interface IPlants {
+  id: number
+  name: string
+  about: string
+  rating: string
+  image: string
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { isLoading, isError, error, data: plants } = useQuery<IPlants[]>({
+    queryKey: ["plants"],
+    queryFn: async () => {
+      const resolve = await axios.get<IPlants[]>("http://localhost:8000/plants");
+      return resolve.data;
+    }
+  })
+
+  if (isLoading) return <p>Loading....</p>
+  if (isError) return <p>Error: {error.message}</p>
+
+  const generatePlants = (): ReactNode => {
+    return plants?.map((plant) => (
+      <li key={plant.id}>
+        <img src={plant.image} alt="image" />
+        <p>{plant.rating}</p>
+        <h1>{plant.name}</h1>
+        <h3>{plant.about}</h3>
+      </li>
+    ))
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <ul>
+      {generatePlants()}
+    </ul>
   )
 }
 
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,21 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   base: "/",
   server: {
     port: 3000,
     open: true,
     hmr: true,
     host: "0.0.0.0",
+    cors: true,
   },
   css: {
     preprocessorOptions: {
       scss: {
         api: "modern",
-      }
-    }
-  }
-})
+      },
+    },
+  },
+});
